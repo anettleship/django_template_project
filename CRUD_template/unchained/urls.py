b31b923816db575e049dadd54a9b06ffe21623bd
Adrian Nettleship <nettlesh@amazon.com>
2023-01-29 21:44:43 +0000

    implement unchained root template to display list of questions from the database

--- CRUD_template/unchained/urls.py
+++ CRUD_template/unchained/urls.py
@@ -3,8 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # example /unchained/
     path('', views.index, name='index'), 
+    # example /unchained/5/
     path('<int:question_id>/', views.detail, name='detail'),
+    # example /unchained/5/results/
     path('<int:question_id>/results/', views.results, name='results'),
+    # example /unchained/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
